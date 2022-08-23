1a095959ba71cf36c6c5f1ae02327adc1e91db62
Mohammad Alhaj <mohammadbasil.alhaj@gmail.com>
2022-08-23 21:52:10 +0300

    repeated-word

--- challenges/challenges-ll-array/hashtable/hashtable.js
+++ challenges/challenges-ll-array/hashtable/hashtable.js
@@ -113,21 +113,14 @@ class HashTable {
           }
 
         return arrayOfKeys
-
     }
+    //********************************************************************************* */
+           // challenge 31 Find Repeated words..
+   
 
 }
 
-const myhashmap = new HashTable(10);
-myhashmap.set('esam', '401d15 student');
-myhashmap.set('ahmad', '401d15 student');
-myhashmap.set('mohamad', '401d15 student');
-myhashmap.set('samah', '401d15 student');
-myhashmap.set('laith', '401d15 student');
-myhashmap.set('shihab', '401d15 student');
 
-
-console.log(myhashmap.keys())
 
 
 module.exports = {
